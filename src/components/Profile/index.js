ffbfb5763370c1eeafea1d454c8f61301a28c675
Marcus Mystrader <mystrader@gmail.com>
2020-05-11 22:42:14 -0300

    final da seção 4

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,27 +1,27 @@
 import React from 'React';
-import { StaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
+import Avatar from '../avatar';
 
-const Profile = () => (
-	<StaticQuery
-		query={graphql`
-			query MySiteMetadata {
-				site {
-					siteMetadata {
-						title
-						position
-						author
-					}
+const Profile = () => {
+	const { site: { siteMetadata: { title, position, description } } } = useStaticQuery(graphql`
+		query MySiteMetadata {
+			site {
+				siteMetadata {
+					title
+					position
+					author
 				}
 			}
-		`}
-		render={({ site: { siteMetadata: { title, position, description } } }) => (
-			<div className="Profile-wrapper">
-				<h1>{title}</h1>
-				<h2>{position}</h2>
-				<p>{description}</p>
-			</div>
-		)}
-	/>
-);
+		}
+	`);
+	return (
+		<div className="Profile-wrapper">
+			<Avatar />
+			<h1>{title}</h1>
+			<h2>{position}</h2>
+			<p>{description}</p>
+		</div>
+	);
+};
 
 export default Profile;
